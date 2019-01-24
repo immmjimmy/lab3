b628670665e233d2e7ce886cd1e7bb752167d79e
immmjimmy <immmjimmy@gmail.com>
2019-01-24 10:42:41 -0800

    Added jQuery and Javascript to the site

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,13 +5,29 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+function projectClick(e) {
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+    	$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+		$(description).fadeOut();
+		$(description).fadeIn();
+    }
+}
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Pound, not Hashtag");
+		$("#testjs").text("Please wait...");
+		$("#testjs").toggleClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
